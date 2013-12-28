b865dd031d96569773a8d5bed29fdf9f1e71738f
Josh Stegmaier <jrs@joshstegmaier.com>
2013-12-28 03:13:28 +0000

    Added generic views with action stream for create and update

--- nimbus/views/generic.py
+++ nimbus/views/generic.py
@@ -17,3 +17,35 @@ class DetailViewWithActionStream(GenerateActionMixin, DetailView):
 
     def get_action_action_object(self, *args, **kwargs):
         return self.get_object()
+
+class CreateViewWithActionStream(GenerateActionMixin, CreateView):
+
+    def form_valid(self, form):
+        to_return = super(CreateViewWithActionStream, self).form_valid(form)
+        self.generate_action()
+        return to_return
+
+    def get_action_actor(self, *args, **kwargs):
+        return self.request.user
+
+    def get_action_verb(self, *args, **kwargs):
+        return 'added'
+
+    def get_action_object(self, *args, **kwargs):
+        return self.get_object()
+
+class UpdateViewWithActionStream(GenerateActionMixin, UpdateView):
+    
+    def form_valid(self, form):
+        to_return = super(UpdateViewWithActionStream, self).form_valid(form)
+        self.generate_action()
+        return to_return
+
+    def get_action_actor(self, *args, **kwargs):
+        return self.request.user
+
+    def get_action_verb(self, *args, **kwargs):
+        return 'updated'
+
+    def get_action_object(self, *args, **kwargs):
+        return self.get_object()
