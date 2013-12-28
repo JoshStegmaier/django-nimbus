b257e9723387d9fd39a8f2cd0da45e7be16b48c2
Josh Stegmaier <jrs@joshstegmaier.com>
2013-12-28 09:22:05 +0000

    Added ability to take arbitary arguments to generate_action helper function

--- nimbus/util/__init__.py
+++ nimbus/util/__init__.py
@@ -1,4 +1,4 @@
 from actstream import action
 
-def generate_action(actor, verb, action_object=None, target=None):
-    action.send(actor, verb=verb, action_object=action_object, target=target)
+def generate_action(actor, verb, action_object=None, target=None, *args, **kwargs):
+    action.send(actor, verb=verb, action_object=action_object, target=target, *args, **kwargs)
